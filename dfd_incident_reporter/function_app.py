48dbf253fd3907d13148ccc4a63abfb5f8dd9088
John Knutson <john.m.knutson@gmail.com>
2023-05-17 23:30:22 -0500

    good start on formatting our dataframe

--- dfd_incident_reporter/function_app.py
+++ dfd_incident_reporter/function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 # import csv
 import io
 import logging
+import matplotlib.pyplot as plt
 import pandas as pd
 from azure.storage.blob import BlobClient, BlobServiceClient
 from azure.identity import DefaultAzureCredential
@@ -21,12 +22,31 @@ def test_function(myblob: func.InputStream, inputblob: str):
     # reader = csv.reader(inputblob, skipinitialspace=True, quoting=csv.QUOTE_ALL)
     # for row in reader:
     #     logging.info(', '.join(row))
+    column_converters = {
+        ''
+    }
+    incident_codes = {
+        100: 'Fire Group.',
+        200: 'Rupture / Explosion.',
+        300: 'Rescue & Emergency Medical Service (EMS)',
+        400: 'Hazardous Condition (no Fire)',
+        500: 'Service Call.',
+        600: 'Good Intent.',
+        700: 'False Alarm & False Call.',
+        800: 'Severe Weather & Natural Disaster Group.'
+    }
     blob_data = io.StringIO(inputblob)
-    df = pd.read_csv(blob_data)
-    drop_columns = [
-        'totalRows','Address','Latitude','Longitude',
-        'Unnamed 18','Unnamed 19','Unnamed 20',
-        'Unnamed 21','Unnamed 22','Unnamed 23', 'Unnamed: 24'
-    ]
-    sanitized_df = df.drop(labels=drop_columns, axis='columns')
+    use_columns = ['IncidentNumber', 'IncidentDate', 'IncidentTime', 'IncidentType']
+    df = pd.read_csv(blob_data, usecols=use_columns,
+                     parse_dates={'IncidentDateTime':['IncidentDate', 'IncidentTime']},
+                     date_format='%d/%m/%Y %H:%M:%S')
+    df['IncidentCode'] = df['IncidentType'].str.split().str[0]
+    df.dropna(subset=['IncidentType'],inplace=True)
+    df['IncidentSeries'] = (df['IncidentCode'].astype(int) / 100).astype(int) * 100
+    df['IncidentCategory'] = df['IncidentSeries'].map(incident_codes)
+    # This is returns sum of unique grouped values, not sure how to pass to matplotlib though
+    df['IncidentCategory'].value_counts()
+    
+    # sanitized_df['IncidentCode'] = sanitized_df['IncidentCode'].astype(int)
+    # sanitized_df['IncidentCodeSeries'] = int(math.floor(sanitized_df['IncidentCode'] / 100.0) * 100)
     logging.info(df)
