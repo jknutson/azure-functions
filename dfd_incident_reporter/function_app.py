763c4843d1ca4e6111f71443e86b30aad91bab7e
John Knutson <john.m.knutson@gmail.com>
2023-05-19 13:16:15 -0500

    Refactor reports (#2)
    * move prefixes to var/constant
    
    * grouping plot works, label needs work
    
    * adjust figure size, titles
    
    * adjust gitignore

--- dfd_incident_reporter/function_app.py
+++ dfd_incident_reporter/function_app.py
@@ -3,24 +3,31 @@ import io
 import logging
 from azure.storage.blob import BlobClient, BlobServiceClient
 from azure.identity import DefaultAzureCredential
-
 # relative import(s)
 import reports
 
+UPLOADS_PREFIX="dfd-uploads"
+REPORTS_PREFIX="dfd-reports"
+
 app = func.FunctionApp()
 
-@app.function_name(name="BlobReportsTrigger")
+@app.function_name(name="BlobTrigger1")
 @app.blob_trigger(arg_name="myblob",
-                  path="dfd-uploads/incidents.csv",
+                  path=f"{UPLOADS_PREFIX}/incidents.csv",
                   connection="")
 @app.blob_input(arg_name="inputblob",
-                path="dfd-uploads/incidents.csv",
+                path=f"{UPLOADS_PREFIX}/incidents.csv",
                 connection="")
-@app.blob_output(arg_name="outputblob",
-                path="dfd-reports/incidents.pdf",
+@app.blob_output(arg_name="outputblobsummary",
+                path=f"{REPORTS_PREFIX}/incidents_summary.pdf",
                 connection="")
-def blob_reports_function(myblob: func.InputStream, inputblob: str, outputblob: func.Out[str]):
+@app.blob_output(arg_name="outputblobmonthly",
+                path=f"{REPORTS_PREFIX}/incidents_monthly.pdf",
+                connection="")
+def test_function(myblob: func.InputStream, inputblob: str,
+                  outputblobsummary: func.Out[str], outputblobmonthly: func.Out[str]):
     incident_data = io.StringIO(inputblob)
-    generated_reports = reports.generate_report(incident_data=incident_data)
+    generated_reports = reports.generate_reports(incident_data=incident_data)
     logging.info(f"generated reports: {generated_reports}")
-    outputblob.set(open(generated_reports[0], 'rb').read())
+    outputblobsummary.set(open(generated_reports[0], 'rb').read())
+    outputblobmonthly.set(open(generated_reports[1], 'rb').read())
--- dfd_incident_reporter/reports.py
+++ dfd_incident_reporter/reports.py
@@ -1,5 +1,7 @@
 import matplotlib
 import pandas as pd
+from matplotlib.ticker import NullFormatter
+from matplotlib.dates import MonthLocator, DateFormatter
 
 INCIDENT_CODES = {
     100: 'Fire Group.',
@@ -11,12 +13,12 @@ INCIDENT_CODES = {
     700: 'False Alarm & False Call.',
     800: 'Severe Weather & Natural Disaster Group.'
 }
+FIGSIZE=(11,8.5)
 
 # agg is a backend that is non-interactive; it can only write to files
 matplotlib.use('agg')
 
-# incident_data should be an InputStream (e.g. from Azure BlobStorageTrigger), or path to a CSV file
-def generate_report(incident_data):
+def generate_reports(incident_data):
     """Generate report(s) from incident data
 
     Parameters:
@@ -25,24 +27,66 @@ def generate_report(incident_data):
     Returns:
     list of str: List of paths to generated report(s)
     """
+
     use_columns = ['IncidentNumber', 'IncidentDate', 'IncidentTime', 'IncidentType']
     df = pd.read_csv(incident_data, usecols=use_columns,
                      parse_dates={'IncidentDateTime':['IncidentDate', 'IncidentTime']},
-                     date_format='%d/%m/%Y %H:%M:%S')
+                     date_format='%m/%d/%Y %H:%M:%S')
     df['IncidentCode'] = df['IncidentType'].str.split().str[0]
     df.dropna(subset=['IncidentType'], inplace=True)
     df['IncidentSeries'] = (df['IncidentCode'].astype(int) / 100).astype(int) * 100
     df['IncidentCategory'] = df['IncidentSeries'].map(INCIDENT_CODES)
 
-    report_path = 'incidents.pdf'
-    fig=df['IncidentCategory'].value_counts().plot(kind='pie').get_figure()
-    fig.savefig(report_path)
+    reports = []
+    reports.extend(generate_summary_report(df))
+    reports.extend(generate_monthly_report(df))
+    return reports
 
-    return [report_path]
+# incident_data should be an InputStream (e.g. from Azure BlobStorageTrigger), or path to a CSV file
+def generate_summary_report(df):
+    """Generate report(s) from dataframe
+
+    Parameters:
+    pandas.DataFrame: dataframe with incident data
+
+    Returns:
+    list of str: List of paths to generated report(s)
+    """
+
+    summary_report_path = 'incidents.pdf'
+    plot=df['IncidentCategory'].value_counts().plot(kind='pie', figsize=FIGSIZE)
+    plot.set(title='Dayton Fire Department - Calls Summary')
+    fig=plot.get_figure()
+    fig.savefig(summary_report_path)
+    return [summary_report_path]
+
+def generate_monthly_report(df):
+    """Generate monthly report(s) from datafram
+
+    Parameters:
+    pandas.DataFrame: dataframe with incident data
+
+    Returns:
+    list of str: List of paths to generated report(s)
+    """
+
+    month_report_path = 'incidents_month.pdf'
+    dg = df.groupby(df.IncidentDateTime.dt.month)['IncidentCategory'].value_counts().unstack().fillna(0)
+    # print(dg.head())
+    # print(dg.info())
+    plot = dg.plot.bar(stacked=True, figsize=FIGSIZE)
+    plot.set(title='Dayton Fire Department - Calls by Month',
+             xlabel='Month', ylabel='Number of Calls')
+    # plot.xaxis.set_major_locator(MonthLocator())
+    # plot.xaxis.set_major_formatter(DateFormatter('%b'))
+    fig=plot.get_figure()
+    fig.savefig(month_report_path)
+    return [month_report_path]
 
 if __name__ == "__main__":
     import sys
-    incident_csv = '/Users/john.knutson/Downloads/incidents.csv'
+    incident_csv = '/Users/john.knutson/Downloads/incidents_2022.csv'
     if len(sys.argv) > 1:
         incident_csv = sys.argv[1]
-    generate_report(incident_data=incident_csv)
+    reports = generate_reports(incident_data=incident_csv)
+    print(f"reports generated: {reports}")
