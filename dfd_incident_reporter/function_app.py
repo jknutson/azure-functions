3272b5f5e5dadbf85cac0941f686c38b5b5c199f
John Knutson <john.m.knutson@gmail.com>
2023-05-19 09:46:00 -0500

    move prefixes to var/constant

--- dfd_incident_reporter/function_app.py
+++ dfd_incident_reporter/function_app.py
@@ -3,23 +3,25 @@ import io
 import logging
 from azure.storage.blob import BlobClient, BlobServiceClient
 from azure.identity import DefaultAzureCredential
-
 # relative import(s)
 import reports
 
+UPLOADS_PREFIX="dfd-uploads"
+REPORTS_PREFIX="dfd-reports"
+
 app = func.FunctionApp()
 
-@app.function_name(name="BlobReportsTrigger")
+@app.function_name(name="BlobTrigger1")
 @app.blob_trigger(arg_name="myblob",
-                  path="dfd-uploads/incidents.csv",
+                  path=f"{UPLOADS_PREFIX}/incidents.csv",
                   connection="")
 @app.blob_input(arg_name="inputblob",
-                path="dfd-uploads/incidents.csv",
+                path=f"{UPLOADS_PREFIX}/incidents.csv",
                 connection="")
 @app.blob_output(arg_name="outputblob",
-                path="dfd-reports/incidents.pdf",
+                path=f"{REPORTS_PREFIX}/incidents.pdf",
                 connection="")
-def blob_reports_function(myblob: func.InputStream, inputblob: str, outputblob: func.Out[str]):
+def test_function(myblob: func.InputStream, inputblob: str, outputblob: func.Out[str]):
     incident_data = io.StringIO(inputblob)
     generated_reports = reports.generate_report(incident_data=incident_data)
     logging.info(f"generated reports: {generated_reports}")
